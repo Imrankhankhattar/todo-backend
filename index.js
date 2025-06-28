895291f20e8fca7d5c358e830cf4c38df0bdf285
ImranKhanKhattar <imrank2018@namal.edu.pk>
2025-06-28 15:29:11 +0500

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js!');
+  res.send('Hello from Node.js, Updated!');
 });
 
 app.listen(PORT, () => {
